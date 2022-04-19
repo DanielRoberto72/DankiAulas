74ba4771fbb9f7c3ec2fff506429178f90130949
Daniel Roberto Pereira <daniel.roberto@surf.com.br>
2022-04-18 21:21:56 -0300

    Adicionando arquivos Projeto Portal de Noticias

---
+++ Projeto_Portal_Noticias/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const bodyParser = require('body-parser')
+const { runInNewContext } = require('vm')
+
+app.engine('html', require('ejs').renderFile)
+app.set('view engine','html')
+app.use('/public', express.static(path.join(__dirname,'public')))
+app.set('views',path.join(__dirname,'/pages'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+app.get('/',(req,res)=>{
+    console.log(req.query)
+
+    if(req.query.busca == null){
+        res.render('home',{})
+    }else{
+        res.send('Você buscou por: '+req.query.busca)
+    }
+})
+
+app.get('/:slug',(req,res)=>{
+    res.send(req.params.slug)
+})
+
+app.listen(5000,()=>{
+    console.log('Servidor rodando com sucesso')
+})
